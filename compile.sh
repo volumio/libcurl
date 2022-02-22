e8481bfec62e9ef3c0d97ce9665339ef37102932
Volumio <info@volumio.org>
2022-02-22 23:32:52 +0100

    curl 7.74

--- compile.sh
+++ compile.sh
@@ -14,12 +14,19 @@ sudo apt-get install -y build-essential automake autoconf debhelper devscripts
 sudo apt-get build-dep -y libcurl4
 
 
+# Version 7.64
 wget http://deb.debian.org/debian/pool/main/c/curl/curl_7.64.0.orig.tar.gz
 tar xf curl_7.64.0.orig.tar.gz
 cd curl-7.64.0/
 wget http://deb.debian.org/debian/pool/main/c/curl/curl_7.64.0-4+deb10u2.debian.tar.xz
 tar xf curl_7.64.0-4+deb10u2.debian.tar.xz
 
+# Version 7.74
+wget http://deb.debian.org/debian/pool/main/c/curl/curl_7.74.0.orig.tar.gz
+tar xf curl_7.74.0.orig.tar.gz
+cd curl-7.74.0/
+wget http://deb.debian.org/debian/pool/main/c/curl/curl_7.74.0-1.2~bpo10+1.debian.tar.xz
+tar xf curl_7.74.0-1.2~bpo10+1.debian.tar.xz
 
 #nano debian/rules
 #remove --with-nghttp2
